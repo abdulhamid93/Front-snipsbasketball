fbde44cce6fcab2d03f2d1ac0b5a62e09ba5460f
Abdulhamid <abdulhamid@mews.me>
2023-11-15 12:29:28 +0200

    .

--- src/components/TeamsList.vue
+++ src/components/TeamsList.vue
@@ -1,79 +1,99 @@
 
 <template>
-    <section
-      class="u-align-center u-clearfix u-container-align-center-lg u-container-align-center-xl u-container-align-center-xs u-palette-5-dark-3 u-section-2"
-      id="carousel_01c2"
-    >
-    
-      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
-        <div class="u-align-center u-container-align-center u-container-style u-group u-shape-rectangle u-group-1 animated customAnimationIn-played" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="250" style="will-change: transform, opacity; animation-duration: 1500ms;">
-          <div class="u-container-layout u-valign-middle u-container-layout-1">
-            <h2 class="u-text u-text-1"> Get started today</h2>
-            <p class="u-large-text u-text u-text-variant u-text-2">We're working on something awesome! <br>Stay tuned for our basketball updates.</p>
-          </div>
+  <section
+    class="u-align-center u-clearfix u-container-align-center-lg u-container-align-center-xl u-container-align-center-xs u-palette-5-dark-3 u-section-2"
+    id="carousel_01c2">
+
+    <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+      <div
+        class="u-align-center u-container-align-center u-container-style u-group u-shape-rectangle u-group-1 animated customAnimationIn-played"
+        data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="250"
+        style="will-change: transform, opacity; animation-duration: 1500ms;">
+        <div class="u-container-layout u-valign-middle u-container-layout-1">
+          <h2 class="u-text u-text-1"> Get started today</h2>
+          <p class="u-large-text u-text u-text-variant u-text-2">We're working on something awesome! <br>Stay tuned for
+            our basketball updates.</p>
         </div>
-        <div class="u-repeater u-repeater-1 teams">
-            <div
-          v-for="(team, index) in teams"
-          :key="index"
+      </div>
+      
+      <div class="u-repeater u-repeater-1 teams">
+        <div v-for="(team, index) in teams" :key="index"
           class="u-align-left u-border-5 u-border-no-bottom u-border-no-left u-border-no-right u-border-palette-1-base u-container-align-center u-container-style u-list-item u-repeater-item u-video-cover u-white u-list-item-5 animated customAnimationIn-played"
-          data-animation-name="customAnimationIn"
-          data-animation-duration="1500"
-          data-animation-delay="500"
-          data-animation-direction=""
-          style="will-change: transform, opacity; animation-duration: 1500ms;"
-        >
+          data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="500"
+          data-animation-direction="" style="will-change: transform, opacity; animation-duration: 1500ms;">
           <div class="u-container-layout u-similar-container u-valign-top u-container-layout-5">
-            <span
-              class="u-align-center u-file-icon u-icon u-text-palette-1-base u-icon-4"
-              data-animation-name=""
-              data-animation-duration="0"
-              data-animation-delay="0"
-              data-animation-direction=""
-            >
+            <span class="u-align-center u-file-icon u-icon u-text-palette-1-base u-icon-4" data-animation-name=""
+              data-animation-duration="0" data-animation-delay="0" data-animation-direction="">
               <img :src="team.logo" alt="" />
             </span>
             <h4 class="u-align-center u-text u-text-9">{{ team.name }}</h4>
-            <a
-              @click="vote(team.name)"
-              class="u-active-black u-btn vote-btn u-button-style u-hover-black u-palette-1-base u-btn-2"
-              >Vote</a
-            >
+            <a @click="vote(team.name)"
+              class="u-active-black u-btn vote-btn u-button-style u-hover-black u-palette-1-base u-btn-2">Vote</a>
           </div>
         </div>
-        </div>
-        
       </div>
-    </section>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  import { defineComponent, ref, onMounted } from 'vue';
 
-  export default {
+    </div>
+  </section>
+</template>
+  
+<script>
+import axios from 'axios';
+import { defineComponent, ref, onMounted } from 'vue';
+
+import { useRoute } from 'vue-router';
+import { baseURL } from '@/config'; 
+export default {
 
   name: 'TeamsList',
-  
-    data() {
-    return {
-      teams:  [
-        {id:1, name: 'AL RIYADI BASKETBALL', logo: 'https://staging.snipsbasketball.com/storage/general/al-riyadi-basketball-logo.png' },
-        {id:2, name: 'BEIRUT', logo: 'https://staging.snipsbasketball.com/storage/general/beirut.png' },
-        {id:3, name: 'Antranik', logo: 'https://staging.snipsbasketball.com/storage/general/antranik.png' },
-        {id:4, name: 'Champville', logo: 'https://staging.snipsbasketball.com/storage/general/champvillelogo.png' },
-        {id:5, name: 'Antonin', logo: 'https://staging.snipsbasketball.com/storage/general/antonin.png' },
-        {id:6, name: 'CS Sagesse', logo: 'https://staging.snipsbasketball.com/storage/general/cs-sagesse-logo.png' },
-        {id:7, name: 'Hmem', logo: 'https://staging.snipsbasketball.com/storage/general/hmemlogo2019.png' },
-        {id:8, name: 'Hoops', logo: 'https://staging.snipsbasketball.com/storage/general/hoops-logo.png' },
-        {id:9, name: 'Mayrouba', logo: 'https://staging.snipsbasketball.com/storage/general/mayrouba.png' },
-        {id:10, name: 'NSA', logo: 'https://staging.snipsbasketball.com/storage/general/nsa.png' },
+  setup() {
+    const route = useRoute();
+    const token = ref('');
 
+    // Set the token initially
+    token.value = route.params.token;
+
+    // Watch for changes to the route and update the token
+    onMounted(() => {
+      token.value = route.params.token;
+      console.log('token.value'+token.value);
+    });
+
+    const tokenExists = ref(false);
+
+    // Check if the token exists
+    onMounted(() => {
+      tokenExists.value = token.value !== undefined && token.value !== null && token.value !== '';
+    });
+
+    // Additional setup logic if needed
+
+    
+  },
+  data() {
+    return {
+      // token:null,
+      // tokenExists:false,
+      teams: [
       ],
     };
   },
 
+  created: function () {
+    this.teams= [
+        { id: 1, name: 'AL RIYADI BASKETBALL', logo: '/storage/general/al-riyadi-basketball-logo.png' },
+        { id: 2, name: 'BEIRUT', logo:  '${baseURL}/storage/general/beirut.png' },
+        { id: 3, name: 'Antranik', logo:  '${baseURL}/storage/general/antranik.png' },
+        { id: 4, name: 'Champville', logo: '${baseURL}/storage/general/champvillelogo.png' },
+        { id: 5, name: 'Antonin', logo:  '${baseURL}/storage/general/antonin.png' },
+        { id: 6, name: 'CS Sagesse', logo:'${baseURL}/storage/general/cs-sagesse-logo.png' },
+        { id: 7, name: 'Hmem', logo: '${baseURL}/storage/general/hmemlogo2019.png' },
+        { id: 8, name: 'Hoops', logo: '${baseURL}/storage/general/hoops-logo.png' },
+        { id: 9, name: 'Mayrouba', logo:  '${baseURL}/storage/general/mayrouba.png' },
+        { id: 10, name: 'NSA', logo:  '${baseURL}/storage/general/nsa.png' },
 
+      ];
+  },
   methods: {
     async vote(teamName) {
       try {
@@ -91,6 +111,6 @@
     },
   },
 };
-  
-  </script>
+
+</script>
   
